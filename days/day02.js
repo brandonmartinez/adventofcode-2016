0dccbf7a62699c485b61aa6f20d1fae3215233c3
Brandon Martinez <me@brandonmartinez.com>
2016-12-02 16:57:27 -0500

    Optimized day 2 puzzle 2. Fixed x/y shift.

--- days/day02.js
+++ days/day02.js
@@ -5,22 +5,14 @@ var puzzle1Grid = [
     ],
     puzzle1Processor = {
         U: function (x, y) {
-            x++;
+            y++;
 
             return {
-                x: (x > 2) ? 2 : x,
-                y: y
+                x: x,
+                y: (y > 2) ? 2 : y
             };
         },
         D: function (x, y) {
-            x--;
-
-            return {
-                x: (x < 0) ? 0 : x,
-                y: y
-            };
-        },
-        L: function (x, y) {
             y--;
 
             return {
@@ -28,33 +20,23 @@ var puzzle1Grid = [
                 y: (y < 0) ? 0 : y
             };
         },
-        R: function (x, y) {
-            y++;
+        L: function (x, y) {
+            x--;
 
             return {
-                x: x,
-                y: (y > 2) ? 2 : y
+                x: (x < 0) ? 0 : x,
+                y: y
+            };
+        },
+        R: function (x, y) {
+            x++;
+
+            return {
+                x: (x > 2) ? 2 : x,
+                y: y
             };
         }
-    };
-
-function calculateNumberPuzzle1(startingNumber, set) {
-    // Calculate the X/Y for the location (we're flipping the X since our numbers don't start at the origin)
-    var x = Math.floor(Math.abs((startingNumber - 9)) / 3),
-        y = (startingNumber - 1) % 3;
-
-    set.forEach(function (direction) {
-        var shifted = puzzle1Processor[direction](x, y);
-        x = shifted.x;
-        y = shifted.y;
-    }, this);
-
-    return puzzle1Grid[x][y];
-}
-
-function calculateNumberPuzzle2(startingLocation, set) {
-    var currentLocation = startingLocation;
-
+    },
     /*
 
     Bounds detection on this shape:
@@ -68,174 +50,104 @@ function calculateNumberPuzzle2(startingLocation, set) {
     Maybe a better way? This is rough but it works.
     
     */
+    puzzle2Grid = [
+        [null, null, 13, null, null],
+        [null, 10, 11, 12, null],
+        [5, 6, 7, 8, 9],
+        [null, 2, 3, 4, null],
+        [null, null, 1, null, null]
+    ],
+    puzzle2Lookup = [
+        [4, 2],
+        [3, 1],
+        [3, 2],
+        [3, 3],
+        [2, 0],
+        [2, 1],
+        [2, 2],
+        [2, 3],
+        [2, 4],
+        [1, 1],
+        [1, 2],
+        [1, 3],
+        [0, 2]
+    ],
+    puzzle2Processor = {
+        U: function (x, y) {
+            if (y !== 2 && !(Math.abs(x) === 1 && y === 1) && Math.abs(x) !== 2) {
+                y++;
+            }
+
+            return {
+                x: x,
+                y: y
+            };
+        },
+        D: function (x, y) {
+            if (y !== -2 && !(Math.abs(x) === 1 && y === -1) && Math.abs(x) !== 2) {
+                y--;
+            }
+
+            return {
+                x: x,
+                y: y
+            };
+        },
+        L: function (x, y) {
+            if (x !== -2 && !(Math.abs(y) === 1 && x === -1) && Math.abs(y) !== 2) {
+                x--;
+            }
+
+            return {
+                x: x,
+                y: y
+            };
+        },
+        R: function (x, y) {
+            if (x !== 2 && !(Math.abs(y) === 1 && x === 1) && Math.abs(y) !== 2) {
+                x++;
+            }
+
+            return {
+                x: x,
+                y: y
+            };
+        }
+    };
+
+function calculateNumberPuzzle1(startingNumber, set) {
+    // Calculate the X/Y for the location (we're flipping the Y since our numbers don't start at the origin)
+    var x = (startingNumber - 1) % 3,
+        y = Math.floor(Math.abs((startingNumber - 9)) / 3);
 
     set.forEach(function (direction) {
-        switch (currentLocation) {
-            case 1:
-                if (direction === 'D') {
-                    currentLocation = 3;
-                }
-                break;
-            case 2:
-                switch (direction) {
-                    case 'R':
-                        currentLocation = 3;
-                        break;
-                    case 'D':
-                        currentLocation = 6;
-                        break;
-                }
-                break;
-            case 3:
-                switch (direction) {
-                    case 'L':
-                        currentLocation = 2;
-                        break;
-                    case 'R':
-                        currentLocation = 4;
-                        break;
-                    case 'U':
-                        currentLocation = 1;
-                        break;
-                    case 'D':
-                        currentLocation = 7;
-                        break;
-                }
-                break;
-            case 4:
-                switch (direction) {
-                    case 'L':
-                        currentLocation = 3;
-                        break;
-                    case 'D':
-                        currentLocation = 8;
-                        break;
-                }
-                break;
-            case 5:
-                if (direction === 'R') {
-                    currentLocation = 6;
-                }
-                break;
-            case 6:
-                switch (direction) {
-                    case 'L':
-                        currentLocation = 5;
-                        break;
-                    case 'R':
-                        currentLocation = 7;
-                        break;
-                    case 'U':
-                        currentLocation = 2;
-                        break;
-                    case 'D':
-                        currentLocation = 10;
-                        break;
-                }
-                break;
-            case 7:
-                switch (direction) {
-                    case 'L':
-                        currentLocation = 6;
-                        break;
-                    case 'R':
-                        currentLocation = 8;
-                        break;
-                    case 'U':
-                        currentLocation = 3;
-                        break;
-                    case 'D':
-                        currentLocation = 11;
-                        break;
-                }
-                break;
-            case 8:
-                switch (direction) {
-                    case 'L':
-                        currentLocation = 7;
-                        break;
-                    case 'R':
-                        currentLocation = 9;
-                        break;
-                    case 'U':
-                        currentLocation = 4;
-                        break;
-                    case 'D':
-                        currentLocation = 12;
-                        break;
-                }
-                break;
-            case 9:
-                if (direction === 'L') {
-                    currentLocation = 8;
-                }
-                break;
-            case 10:
-                switch (direction) {
-                    case 'R':
-                        currentLocation = 11;
-                        break;
-                    case 'U':
-                        currentLocation = 6;
-                        break;
-                }
-                break;
-            case 11:
-                switch (direction) {
-                    case 'L':
-                        currentLocation = 10;
-                        break;
-                    case 'R':
-                        currentLocation = 12;
-                        break;
-                    case 'U':
-                        currentLocation = 7;
-                        break;
-                    case 'D':
-                        currentLocation = 13;
-                        break;
-                }
-                break;
-            case 12:
-                switch (direction) {
-                    case 'L':
-                        currentLocation = 11;
-                        break;
-                    case 'U':
-                        currentLocation = 8;
-                        break;
-                }
-                break;
-            case 13:
-                if (direction === 'U') {
-                    currentLocation = 11;
-                }
-                break;
-        }
+        var shifted = puzzle1Processor[direction](x, y);
+        x = shifted.x;
+        y = shifted.y;
     }, this);
 
-    return currentLocation;
+    return puzzle1Grid[y][x];
 }
 
-function puzzle1(inputs) {
+function calculateNumberPuzzle2(startingNumber, set) {
+    var x = puzzle2Lookup[startingNumber - 1][1] - 2,
+        y = puzzle2Lookup[startingNumber - 1][0] - 2;
+
+    set.forEach(function (direction) {
+        var shifted = puzzle2Processor[direction](x, y);
+        x = shifted.x;
+        y = shifted.y;
+    }, this);
+
+    return puzzle2Grid[y + 2][x + 2];
+}
+
+function puzzleProcessor(inputs, calculator) {
     var location = 5,
         finalNumber = '';
 
     for (var i = 0; i < inputs.length; i++) {
-        location = calculateNumberPuzzle1(location, inputs[i]);
-
-        finalNumber += location.toString();
-    }
-
-    return finalNumber;
-}
-
-function puzzle2(inputs) {
-    var location = 5,
-        finalNumber = '';
-
-    for (var i = 0; i < inputs.length; i++) {
-        location = calculateNumberPuzzle2(location, inputs[i]);
+        location = calculator(location, inputs[i]);
 
         finalNumber += location.toString(16);
     }
@@ -266,15 +178,15 @@ module.exports = function () {
         inputs = [splitInput1, splitInput2, splitInput3, splitInput4, splitInput5];
 
     // TEST DATA
-    //inputs = [splitInput1, splitInput2, splitInput3, splitInput4];
+    // inputs = [splitInput1, splitInput2, splitInput3, splitInput4];
 
     // Puzzle 1
-    var puzzle1Answer = puzzle1(inputs);
+    var puzzle1Answer = puzzleProcessor(inputs, calculateNumberPuzzle1);
     // Should be 18843
     console.log('--- Puzzle 1 - Answer: ' + puzzle1Answer + '.');
 
     // Puzzle 2
-    var puzzle2Answer = puzzle2(inputs);
+    var puzzle2Answer = puzzleProcessor(inputs, calculateNumberPuzzle2);
     // Should be 67BB9
-    console.log('--- Puzzle 2 - Answer: ' + puzzle2Answer + '.');
+    console.log('--- Puzzle 2 - Answer: ' + puzzle2Answer.toUpperCase() + '.');
 };
