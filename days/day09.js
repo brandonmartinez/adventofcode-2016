6153f56a7ea09ef70670bbf8b977a5766b99930f
Brandon Martinez <me@brandonmartinez.com>
2016-12-10 23:49:24 -0500

    Works with sample, not with given input (memory errror)

--- days/day09.js
+++ days/day09.js
@@ -1,7 +1,7 @@
 var fs = require('fs'),
     path = process.cwd();
 
-function decompressData(input) {
+function decompressDataVersionOne(input) {
     var decompressedOutput = '',
         i = 0;
 
@@ -41,14 +41,59 @@ function decompressData(input) {
     return decompressedOutput;
 }
 
+function decompressDataVersionTwoFileLength(input) {
+    var decompressedLength = 0,
+        i = 0;
+
+    while (i < input.length) {
+        // Grab the working substring
+        var markerRegex = /\((\d+)x(\d+)\)/,
+            inputSubstring = input.substring(i),
+            m = markerRegex.exec(inputSubstring),
+            newOutputAddition = '',
+            previousLength;
+
+        if (m) {
+            // If the next marker isn't at the start of the substring, grab the characters as data and append
+            if (m.index !== 0) {
+                newOutputAddition += inputSubstring.substring(0, m.index);
+                inputSubstring = inputSubstring.substring(m.index);
+            }
+
+            // Get length and repeat
+            var charactersToRepeat = parseInt(m[1], 10),
+                repeatNumber = parseInt(m[2], 10),
+                offset = m[0].length,
+                repeatSubstring = inputSubstring.substring(offset, charactersToRepeat + offset);
+
+            for (var r = 0; r < repeatNumber; r++) {
+                newOutputAddition += repeatSubstring;
+            }
+
+            while (newOutputAddition.indexOf('(') !== -1) {
+                newOutputAddition = decompressDataVersionOne(newOutputAddition);
+            }
+
+            i += m.index + offset + charactersToRepeat;
+        } else {
+            newOutputAddition += inputSubstring;
+            i += inputSubstring.length + 1;
+        }
+
+        decompressedLength += newOutputAddition.length;
+    }
+
+    return decompressedLength;
+}
+
 function puzzle1(input) {
-    var processedInput = decompressData(input);
-    //console.log(processedInput);
+    var processedInput = decompressDataVersionOne(input);
     return processedInput.length;
 }
 
 function puzzle2(input) {
-    return '';
+    var processedInputLength = decompressDataVersionTwoFileLength(input);
+    return processedInputLength;
 }
 
 
